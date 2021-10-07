8cc75ce00b7ba81ea3050349f7b7f55189b5bcfc
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-10-07 19:14:13 +0530

    improved the ui

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,8 @@ export default class Header extends React.Component {
             <React.Fragment>
                 <h1 className="logo">
                     <a href="/" data-testid="header-text">
-                        NASA Media Search
+                    <img src="http://1.bp.blogspot.com/-3xCvi76N0fs/UJhTZJWDRwI/AAAAAAABUbg/nr5DNpUiBjk/s1600/Nasa_Logo.jpg"/>
+                         Media Search
                     </a>
                 </h1>
             </React.Fragment>
